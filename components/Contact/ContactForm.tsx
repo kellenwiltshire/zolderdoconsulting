e104af74d6f50a7c9eaebb36b5b9ec6a56bbd861
Kellen Wiltshire <github@kellenwiltshire.com>
2026-09-22 18:14:06 -0400

    Make address/role number optional

--- components/Contact/ContactForm.tsx
+++ components/Contact/ContactForm.tsx
@@ -167,7 +167,6 @@ function ContactForm() {
 												id='address'
 												name='address'
 												type='address'
-												required
 												autoComplete='address'
 												aria-describedby='address'
 												className='block w-full rounded-md bg-white px-3.5 py-2 text-base text-black outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600'
